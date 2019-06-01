466ffc4504efb02c05c34eaa4bcddd0bc16ef082
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-01 13:27:18 +0700

    add graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-// import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
 
-// const client = new ApolloClient({
-//   uri: 'localhost:5000/graphql',
-// });
+const client = new ApolloClient({
+  uri: 'localhost:5000/graphql',
+});
 
-// const App = () => {};
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app</h2>
     </div>
-  );
-};
+  </ApolloProvider>
+);
 
 export default App;
