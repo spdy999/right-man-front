48d76038914770ad9475157f2bd22cfe0c1af761
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-01 17:11:41 +0700

    fix PokemonSearch view

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -3,6 +3,10 @@ import User from '../interfaces/User.interface';
 
 interface SearchState {
   error: boolean;
+  pokemon: Pokemon;
+}
+
+interface Pokemon {
   name: string;
   numberOfAbilities: number;
   baseExperience: number;
@@ -15,10 +19,7 @@ export class PokemonSearch extends Component<User, SearchState> {
     super(props);
     this.state = {
       error: false,
-      name: '',
-      numberOfAbilities: 0,
-      baseExperience: 0,
-      imageUrl: '',
+      pokemon: null,
     };
     this.pokemonRef = React.createRef();
   }
@@ -32,35 +33,31 @@ export class PokemonSearch extends Component<User, SearchState> {
       res.json().then(data => {
         this.setState({
           error: false,
-          name: data.name,
-          numberOfAbilities: data.abilities.length,
-          baseExperience: data.base_experience,
-          imageUrl: data.sprites.front_default,
+          pokemon: {
+            name: data.name,
+            numberOfAbilities: data.abilities.length,
+            baseExperience: data.base_experience,
+            imageUrl: data.sprites.front_default,
+          },
         });
       });
     });
   };
   render() {
     const { name: userName, numberOfPokemons } = this.props;
-    const {
-      error,
-      name,
-      numberOfAbilities,
-      baseExperience,
-      imageUrl,
-    } = this.state;
+    const { error, pokemon } = this.state;
 
     let resultMarkup;
 
     if (error) {
       resultMarkup = <p>Pokemon not found, please try again</p>;
-    } else {
+    } else if (this.state.pokemon) {
       resultMarkup = (
         <div>
-          <img src={imageUrl} alt="pokemon" className="pokemon-image" />
+          <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
           <p>
-            {name} has {numberOfAbilities} abilities and {baseExperience} base
-            experience points
+            {pokemon.name} has {pokemon.numberOfAbilities} abilities and{' '}
+            {pokemon.baseExperience} base experience points
           </p>
         </div>
       );
