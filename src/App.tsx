9960e457c0dce8a9309659bae1ff9071a099ec60
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-02 01:00:02 +0700

    test routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import './App.css';
 import ApolloClient from 'apollo-boost';
-import MyForm from './components/MyForm';
+import BasicExample from './components/Example';
+// import MyForm from './components/MyForm';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
@@ -12,9 +13,10 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div style={{ textAlign: 'center' }}>
+        <BasicExample />
+        {/* <div style={{ textAlign: 'center' }}>
           <MyForm onSubmit={() => {}} />
-        </div>
+        </div> */}
       </ApolloProvider>
     );
   }
--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
+// import { Formik, Form } from 'formik';
 
-interface Props {
-  onSubmit: () => void;
-}
+// interface Props {
+//   onSubmit: () => void;
+// }
 
-export default function MyForm(props: Props) {
-  return <TextField />;
-}
+// export default function MyForm(props: Props) {
+//   return (
+//     <Formik
+//       initialValues={{ firstName: '', lastName: '', email: '' }}
+//       onSubmit={values => {
+//         onSubmit(values);
+//       }}
+//     >
+//       {() => {
+//         <Form>
+//           <TextField />;
+//         </Form>;
+//       }}
+//     </Formik>
+//   );
+// }
