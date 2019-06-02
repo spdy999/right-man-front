b543827bb772e8b53549b6e7d6418655d1aa7082
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-02 22:11:37 +0700

    update login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,23 +1,75 @@
 import React, { Component } from 'react';
 
-class LoginForm extends Component {
+interface LoginState {
+  username: string;
+  password: string;
+  currentUser: string;
+  message: string;
+}
+class LoginForm extends Component<null, LoginState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: null,
+      password: null,
+      currentUser: null,
+      message: null,
+    };
+  }
+  onChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'username': {
+        console.log(name);
+        this.setState({ username: value });
+        break;
+      }
+      case 'password': {
+        console.log(name);
+        this.setState({ password: value });
+        break;
+      }
+    }
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { username, password } = this.state;
+    console.log('onsubmit', username, password);
+    // TODO: implement signInWithEmailAndPassword()
+  };
+
   render() {
     return (
       <section className="section container">
         <div className="columns is-centered">
           <div className="column is-half">
-            <form>
+            <form onSubmit={this.onSubmit}>
+              {/* <form> */}
               <div className="field">
                 <label className="label">Username</label>
                 <div className="control">
-                  <input className="input" type="username" name="username" />
+                  <input
+                    className="input"
+                    type="username"
+                    name="username"
+                    // value={this.state.username}
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <label className="label">Password</label>
                 <div className="control">
-                  <input className="input" type="password" name="password" />
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
 
