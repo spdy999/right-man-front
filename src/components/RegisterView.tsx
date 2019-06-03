08c562da76a17f5e8ed69e7f474b57ece9e1a9c4
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-03 16:26:46 +0700

    success using mutation in frontend

--- src/components/RegisterView.tsx
+++ src/components/RegisterView.tsx
@@ -1,8 +1,78 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { RouteComponentProps } from 'react-router-dom';
+import { async } from 'q';
 
-export class RegisterView extends Component {
+const registerMutation = gql`
+  mutation RegisterMutation($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      email
+      token
+    }
+  }
+`;
+// fix this.props.history
+export class RegisterView extends Component<RouteComponentProps<{}>> {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = (e: any) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({
+      [name]: value,
+    });
+  };
   render() {
-    return <div>Register View</div>;
+    const { email, password } = this.state;
+    return (
+      <Mutation mutation={registerMutation}>
+        {(register, result) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <div>
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <button
+                onClick={async () => {
+                  register({ variables: this.state });
+                  console.log('registered');
+                  console.log(result);
+                  this.props.history.push('/login');
+                }}
+              >
+                register
+              </button>
+            </div>
+          </div>
+        )}
+      </Mutation>
+    );
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Routes from './Routes';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
